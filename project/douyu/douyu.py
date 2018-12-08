3bdfca7ecc0f69b60b51d8a9f90f6a2c5a8e02c0
ylfeng250 <1119030015@qq.com>
2018-12-08 15:54:08 +0800

    斗鱼弹幕数据

---
+++ project/douyu/douyu.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2018/12/08 11:50:05
+@Author  :   我的孙女叫小芳 
+@Contact :   1119030015@qq.com
+'''
+
+# here put the import lib
+import socket
+import struct
+import time
+import re
+from threading import Thread
+import requests
+
+class Douyu():
+    def __init__(self,url="475252"):
+        print("==============start================")
+        # 房间号
+        self.url = str(url)
+        # ip
+        self.ip = "openbarrage.douyutv.com"
+        # 端口号
+        self.port = 8601
+        self.get_info()
+
+        print(f"=======您现在正在接收{self.room_id}房间的弹幕数据==========")
+    def get_info(self):
+        base_url = "http://open.douyucdn.cn/api/RoomApi/room/"
+        if self.url.isdigit():
+            self.room_id = self.url
+        else:
+            self.room_id = self.url.strip().split('/')[-1]
+        link = base_url + self.room_id
+        headers = {
+            "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+        }
+        res = requests.get(link,headers=headers).json()
+        # print(res)
+        if res["error"] == 101:
+            print(res.data)
+            print("请输入房间号或者带房间号的直播间地址")
+        else:
+            owner_name = res["data"]["owner_name"]
+            room_name = res["data"]["room_name"]
+            print(f"欢迎来到  {owner_name}  的直播间——房间名称  {room_name}")
+        
+
+    def run(self):
+        """
+        启动脚本
+        """
+        self.connect()
+        self.login()
+        t = Thread(target=self.keep_alive,daemon=True) ## 主线程退出后直接退出
+        t.start()
+        self.recive_msg()
+
+
+    def connect(self):
+        """
+        连接弹幕服务器
+        第三方接入弹幕服务器列表：
+        IP 地址： openbarrage.douyutv.com 端口： 8601
+        """
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.socket.connect((self.ip,self.port))
+
+    def send_msg(self,msg):
+        msg = msg.encode()
+        code = 689 # 客户端发送给弹幕服务器
+        msg_length = len(msg) + 8  # 加 8 就行 不行换12
+        head = struct.pack('i',msg_length) + struct.pack('i',msg_length) + struct.pack('i',code)
+        self.socket.sendall(head+msg)
+
+    def login(self):
+        """
+        登录
+        """
+        login = f"type@=loginreq/roomid@={self.room_id}/\x00"
+        # login = login.encode('utf-8')
+        self.send_msg(login)
+        joingroup = f"type@=joingroup/rid@={self.room_id}/gid@=-9999/\x00"
+        # joingroup = joingroup.encode('utf-8')
+        self.send_msg(joingroup)
+    
+    def recive_msg(self):
+        """
+        接收消息
+        """
+        pattern = re.compile(b'type@=chatmsg/(.*?)\x00')
+        while True:
+            content = self.socket.recv(1024)
+            content = pattern.search(content)
+            if content:
+                msg_obj = self.parse(content.group(1))
+                nick_name = msg_obj["nn"]
+                txt = msg_obj["txt"]
+                # repin = msg_obj["repin"]
+                print(f"{nick_name}\t\tsend\t\t{txt}")
+            else:
+                pass
+    
+
+    def keep_alive(self):
+        """
+        保持于弹幕服务器的连接 每隔45秒发送心跳信息
+        """
+        while True:
+            t = str(int(time.time()))
+            msg = f"type@=keeplive/tick@={t}/\x00"
+            self.send_msg(msg)
+            time.sleep(45)        
+
+    def parse(self,content):
+        """
+        解析返回的消息
+        存在不能转为utf-8的表情符号
+        UnicodeDecodeError: 'utf-8' codec can't decode byte 0xed in position 75: invalid continuation byte
+        """
+        content_obj = {}
+        # print(content)
+        content = content.decode(errors='replace').strip()
+        tmp_kv_list = content.split('/')
+        for kv in tmp_kv_list:
+            kv = kv.strip()
+            if len(kv) == 0:
+                continue
+            kv = kv.split("@=")
+            content_obj[kv[0]]=kv[1]
+        return content_obj
+    
+
+
+
+if __name__ == "__main__":
+    douyu = Douyu(475252)
+    douyu.run()
---
+++ project/douyu/readme.md
@@ -0,0 +1,94 @@
+# 斗鱼弹幕数据抓取
+
+主要参考斗鱼的文档 http://dev-bbs.douyutv.com/forum.php?mod=forumdisplay&fid=37
+
+![1544254773054](assets/1544254773054.png)
+
+
+
+## 建立连接
+
+通过socket与弹幕服务器建立连接
+
+```
+第三方接入弹幕服务器列表：
+IP 地址： openbarrage.douyutv.com 端口： 8601
+```
+
+## 发送消息
+
+协议组成
+
+![1544255096219](assets/1544255096219.png)
+
+**消息长度**： 4 字节小端整数，表示整条消息（包括自身）长度（字节数）。消息长度出现两遍，二者相同
+
+**消息类型**： 2 字节小端整数， 表示消息类型。取值如下：
+689 	客户端发送给弹幕服务器的文本格式数据
+690 	弹幕服务器发送给客户端的文本格式数据
+
+**加密字段**： 暂时未用，默认为 0。
+**保留字段**： 暂时未用，默认为 0。
+
+**数据部分**：斗鱼独创序列化文本数据，结尾必须为‘\0’。
+
+msg是要发送的数据部分，实际是要加上长度4头部4+4个字节长度的，也就是12个但是实际发出去的时候会自动加4个，所以+8
+
+```python
+msg = msg.encode()
+code = 689 # 客户端发送给弹幕服务器
+msg_length = len(msg) + 8  # 加 8 就行 不行换12
+head = struct.pack('i',msg_length) + struct.pack('i',msg_length) + struct.pack('i',code)
+self.socket.sendall(head+msg)
+```
+
+## 登录
+
+登录弹幕服务器，不需要输入用户名和密码，应该是和游客模式一样
+
+```python
+login = f"type@=loginreq/roomid@={self.room_id}/\x00"
+```
+
+## 加入弹幕组
+
+默认加入弹幕组`-9999`,海量弹幕组
+
+```python
+joingroup = f"type@=joingroup/rid@={self.room_id}/gid@=-9999/\x00"
+```
+
+## 心跳消息
+
+保持于弹幕服务器的连接 每隔45秒发送心跳信息
+
+```python
+msg = f"type@=keeplive/tick@={t}/\x00"
+```
+
+## 解析消息
+
+正则匹配返回的内容，然后转成Python对象，其中会存在不能转为utf-8的表情符号`UnicodeDecodeError: 'utf-8' codec can't decode byte 0xed in position 75: invalid continuation byte` 忽略这个错误
+
+```python
+content_obj = {}
+# print(content)
+content = content.decode(errors='replace').strip()
+tmp_kv_list = content.split('/')
+for kv in tmp_kv_list:
+    kv = kv.strip()
+    if len(kv) == 0:
+        continue
+    kv = kv.split("@=")
+    content_obj[kv[0]]=kv[1]
+return content_obj
+    
+```
+
+
+
+## 之后可能会做
+
+暂时就是为了好玩，没有做持久化处理，之后可能会做
+
+​        
